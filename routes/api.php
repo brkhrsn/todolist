a48bd5ecfe35f9a468234025610c2315cdc0ec25
brkhrsn <burakhorasan@gmail.com>
2022-10-15 15:05:21 +0300

    first

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/items', [\App\Http\Controllers\ItemController::class, 'index']);
+Route::prefix('/item')->group(function (){
+    Route::post('/store', [\App\Http\Controllers\ItemController::class,'store']);
+    Route::put('/{id}', [\App\Http\Controllers\ItemController::class,'update']);
+    Route::delete('/{id}', [\App\Http\Controllers\ItemController::class,'destroy']);
+
 });
