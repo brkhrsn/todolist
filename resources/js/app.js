a90d9b2143a562b64c5c9a16adb187d2ff842a2d
brkhrsn <burakhorasan@gmail.com>
2022-10-15 15:20:23 +0300

    first

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+import App from './vue/app'
+export const eventBus = createApp(App)
+createApp(App).mount('#app')
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]).vue();
